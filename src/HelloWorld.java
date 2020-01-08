1e6c61726d85eecb6b3ed42d315e47336c6cec48
lzontar <44132336+lzontar@users.noreply.github.com>
2020-01-08 02:26:33 +0100

    Added example that works

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static int sum(int a, int b) {
+        return ((a + b) * 3) + (17 - 8 / 4);
+    }
+}
